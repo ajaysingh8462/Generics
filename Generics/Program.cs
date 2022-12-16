5cc1c35272f73827118f6d8a8d8d6e34a41fab6a
ajaysingh8462 <ajaysingh84625@gmail.com>
2022-12-16 11:45:38 +0530

    [ajay]-Find max no for tc1.1

---
+++ Generics/Program.cs
@@ -0,0 +1,19 @@
+﻿using Generics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenricDemo
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+
+            int maxint = FindMax.FindIntMax(3, 2, 1);
+            Console.WriteLine("The maximum number of in  is :" + maxint);
+        }
+    }
+}
