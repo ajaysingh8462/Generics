46b34a70eecb12607f4df4173c9ffa575f9b025b
ajaysingh8462 <ajaysingh84625@gmail.com>
2022-12-19 00:45:46 +0530

    [ajay]-using genric mathod all test case

--- Generics/FindMax.cs
+++ Generics/FindMax.cs
@@ -8,7 +8,7 @@ namespace Generics
 {
     internal class FindMax
     {
-        public static int FindIntMax(int num1, int num2, int num3)
+        public static T FindMaxNUM<T>(T num1, T num2, T num3) where T : IComparable
         {
             if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0)
             {
@@ -22,39 +22,7 @@ namespace Generics
             {
                 return num3;
             }
-            return 0;
-        }
-        public static float FindFloatMax(float num1, float num2, float num3)
-        {
-            if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0)
-            {
-                return num1;
-            }
-            if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0)
-            {
-                return num2;
-            }
-            if (num3.CompareTo(num1) > 0 && num3.CompareTo(num2) > 0)
-            {
-                return num3;
-            }
-            return 3;
-        }
-        public static String FindStringMax(string A, string B, string C)
-        {
-            if (A.CompareTo(B) > 0 && A.CompareTo(C) > 0)
-            {
-                return A;
-            }
-            if (B.CompareTo(A) > 0 && B.CompareTo(C) > 0)
-            {
-                return B;
-            }
-            if (C.CompareTo(A) > 0 && C.CompareTo(B) > 0)
-            {
-                return C;
-            } 
-            return null;
+            return default;
         }
     }
 }
--- Generics/Program.cs
+++ Generics/Program.cs
@@ -12,28 +12,34 @@ namespace Generics
     {
         public static void Main(string[] args)
         {
-
-            int maxint = FindMax.FindIntMax(3, 2, 1);
+            Console.WriteLine("Test case 1 find max of int (3,2,1)");
+            int maxint = FindMax.FindMaxNUM<int>(3, 2, 1);
             Console.WriteLine("The maximum number of in  is :" + maxint);
+            Console.WriteLine("Test case 2 find max of int (1,3,1)");
+            int Maxint = FindMax.FindMaxNUM<int>(1, 3, 2);
+            Console.WriteLine("The maximum number of in  is :" + Maxint);
+            Console.WriteLine("Test case 3 find max of int (1,2,3)");
+            int maxInt = FindMax.FindMaxNUM<int>(1, 2, 3);
+            Console.WriteLine("The maximum number of in  is :" + maxInt);
 
-            Console.WriteLine("Test case 1 find max of float (3.1,.1,1.1)");
-            float Maxfloat = FindMax.FindFloatMax(3.1f, 2.1f, 1.1f);
+            Console.WriteLine("Test case 1 find max of float (3.1,2.1,1.1)");
+            float Maxfloat = FindMax.FindMaxNUM<float>(3.1f, 2.1f, 1.1f);
             Console.WriteLine("The maximum number of float is :" + Maxfloat);
-            Console.WriteLine("Test case 2 find max of float (1.1,3.1,1.1)");
-            float maxfloat = FindMax.FindFloatMax(1.1f, 3.1f, 2.1f);
+            Console.WriteLine("Test case 2 find max of float (1.1,3.1,2.1)");
+            float maxfloat = FindMax.FindMaxNUM<float>(1.1f, 3.1f, 2.1f);
             Console.WriteLine("The maximum number of float is :" + maxfloat);
             Console.WriteLine("Test case 3 find max of float (1.1,2.1,3.1)");
-            float maxFloat = FindMax.FindFloatMax(1.1f, 2.1f, 3.1f);
+            float maxFloat = FindMax.FindMaxNUM<float>(1.1f, 2.1f, 3.1f);
             Console.WriteLine("The maximum number of float is :" + maxFloat);
 
             Console.WriteLine("Test case 1 find max value of string (Peach,Banana, Apple)");
-            string Maxstring = FindMax.FindStringMax("Peach", "Banana", "Apple");
+            string Maxstring = FindMax.FindMaxNUM<string>("Peach", "Banana", "Apple");
             Console.WriteLine("The maximum value of string is :" + Maxstring);
             Console.WriteLine("Test case 2 find max value of string (Banana,Peach, Apple)");
-            string MaxString = FindMax.FindStringMax("Banana", "Peach", "Apple");
+            string MaxString = FindMax.FindMaxNUM<string>("Banana", "Peach", "Apple");
             Console.WriteLine("The maximum number of float is :" + MaxString);
             Console.WriteLine("Test case 2 find max value of string (Banana,Apple, Peach)");
-            string Max_String = FindMax.FindStringMax("Banana", "Apple", "Peach");
+            string Max_String = FindMax.FindMaxNUM<string>("Banana", "Apple", "Peach");
             Console.WriteLine("The maximum number of float is :" + Max_String);
 
 
